534edff02deca5e1404fe149b2c68f6e3590e7bf
official16Dec <officialsunil16@gmail.com>
2025-09-03 18:33:27 +0530

    Project root directory reconfigured

--- androidApp/src/main/java/com/linus/muzicvia/android/MainActivity.kt
+++ androidApp/src/main/java/com/linus/muzicvia/android/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.linus.muzicvia.android
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.linus.muzicvia.Greeting
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
@@ -28,7 +35,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingView(text: String) {
-    Text(text = text)
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        val painter = painterResource(id = R.drawable.splash_bg)
+        Image(
+            painter = painter,
+            contentDescription = "Description for the painter image",
+            modifier = Modifier.fillMaxSize(),
+        )
+        Text(text)
+    }
 }
 
 @Preview
